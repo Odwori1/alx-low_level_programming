a0ff57ced3a03c583c5ce2b68b36532bf61e4a4c
Odwori1 <odwori.okuku@gmail.com>
2023-02-17 11:42:32 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * Analogy: character in reverse order
+ * Return: Always 0 (Seun == Success)
+ */
+int main(void)
+{
+	char s;
+
+	for (s = 'z'; s >= 'a'; s--)
+	{
+	putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
