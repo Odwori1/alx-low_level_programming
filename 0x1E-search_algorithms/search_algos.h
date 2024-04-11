e2f1d04ba0d830e3623e27771b6597f16f0f2732
Odwori1 <odwori.okuku@gmail.com>
2024-04-11 05:34:24 -0700

    0-linear.c

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGO__H
+#define SEARCH_ALGO__H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+
+#endif
